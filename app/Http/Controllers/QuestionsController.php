0fa6a0aafa5073027870c87f2c91ff501a833b66
PeacefulNoob <59912238+PeacefulNoob@users.noreply.github.com>
2020-03-26 20:06:35 +0100

    Laravellek19

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
@@ -14,8 +15,13 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
-        return view('questions.index',compact('questions'));
+        /*  \DB::enableQueryLog();
+        $questions = Question::with('user')->latest()->paginate(10);
+         view('questions.index',compact('questions'))->render();
+       dd  (\DB::getQueryLog()); */
+
+        $questions = Question::with('user')->latest()->paginate(10);
+        return view('questions.index', compact('questions'));
     }
 
     /**
@@ -25,7 +31,9 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create', compact('question'));
     }
 
     /**
@@ -34,9 +42,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+         $request->user()->questions()->create($request->only('title', 'body'));
+
+        return redirect()->route('questions.index')->with('success', "Your question has been submitted");
     }
 
     /**
@@ -48,6 +58,9 @@ class QuestionsController extends Controller
     public function show(Question $question)
     {
         //
+        $question->increment('views');
+
+        return view('questions.show' , compact('question'));
     }
 
     /**
@@ -58,7 +71,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        //
+       return view("questions.edit" ,compact('question'));
     }
 
     /**
@@ -68,9 +81,11 @@ class QuestionsController extends Controller
      * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Question $question)
+    public function update(AskQuestionRequest $request, Question $question)
     {
-        //
+     
+        $question->update($request->only('title','body'));
+        return redirect()->route('questions.index')->with('success', "Your question has been updated");
     }
 
     /**
@@ -81,6 +96,8 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return redirect('/questions')->with('success', "Your question has been deleted.");
     }
 }
--- app/Question.php
+++ app/Question.php
@@ -5,20 +5,39 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
-{  
-   protected $fillable = ['title', 'body'];
-   
-   public function user()
-   {
-       return $this->belongsTo(User::class);
-   }
+{
+    protected $fillable = ['title', 'body'];
 
-   public function setTitleAttribute($value)
-   {
-       $this->attributes['title'] = $value;
-       $this->attributes['slug'] = str_slug($value);
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
-   }
-/*    $question = Question::find(1);
-   $question->user->name */
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
+    public function getUrlAttribute()
+    {
+        return route("questions.show",$this->slug);
+        // return route("question.show", $this->id);
+    }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
+
+ 
 }
